0feef910be05812cd134ccb74e0598e4069802dd
armaBurton <armanarma@armaburton.com>
2021-12-02 12:05:56 -0800

    HTML/CSS complete, starting JavaScript

---
+++ script/script.js
@@ -0,0 +1,22 @@
+// import functions and grab DOM elements
+import { createCountString } from './utils.js';
+// let state
+
+// set event listeners 
+  // get user input
+  // use user input to update state 
+  // update DOM to reflect the new state
+
+//pure
+
+
+//impure
+
+function displayStats(){
+
+}
+
+function displaySlogans(){
+
+}
+
