6c9d4d4e2db756e2739ed4114be2e2e562d66a8c
armaBurton <armanarma@armaburton.com>
2021-12-03 15:32:51 -0800

    dynamic loading of HTML from array of object working, but it's ugly

--- script/script.js
+++ script/script.js
@@ -4,7 +4,7 @@ import { createCountString } from './utils.js';
 //stretch array of objects
 const arrayOfObjects = [
     {
-        name: `CASTLE`,
+        name: `castle`,
         drop: [
             `french`,
             `german`,
@@ -30,14 +30,7 @@ const arrayOfObjects = [
 ];
 
 let populateArr = [];
-let castleDropdown,
-    castleImage = document.getElementById(`castle-image`);
-
-//grab dropdown elements;
-const 
-    // castleDropdown = document.getElementById(`castle-dropdown`),
-    downtownDropdown = document.getElementById(`downtown-dropdown`),
-    marsDropdown = document.getElementById(`mars-dropdown`);
+let castleImage = document.getElementById(`castle-image`);
 
 //Grab image divs
 const
@@ -57,17 +50,18 @@ const
     sloganButton = document.getElementById(`slogan-button`),
     sloganShow = document.getElementById(`slogan-show`);
     
-//grab columns for populating dropdowns
-const 
-    parent1 = document.getElementById(`col1`),
-    child1 = document.getElementById(`castle-image`),
-    col2 = document.getElementById(`col2`),
-    col3 = document.getElementById(`col3`);
+
 
 // let state
 let castle = 0,
     downtown = 0,
     mars = 0;
+
+let dropdown = [],
+    imgArr = [],
+    parent,
+    child;
+
 
 let countStr = ``;
 
@@ -75,40 +69,75 @@ let sloganArr = [];
 
 // set event listeners 
 //init load assign initial values to variables !!!no blank divs!!!
+
 window.addEventListener('load', () => {
-    let h2 = document.createElement(`h2`);
-    h2.classList.add(`text-shadow`, `title-case`);
-    h2.setAttribute(`id`, `castle-name`);
-    h2.textContent = arrayOfObjects[0].name;
-    // let castleName = document.getElementById(`castle-name`);
-    let select = document.createElement(`select`);
-    select.setAttribute(`name`, `castle`);
-    select.setAttribute(`id`, `castle-dropdown`);
-    populateArr.push(h2);
-    for (let d of arrayOfObjects[0].drop){
-        let option = document.createElement(`option`);
-        option.classList.add(`title-case`);
-        option.setAttribute(`value`, d);
-        option.textContent = titleCase(d);
-        select.append(option);
+    for (let i = 0; i < 3; i++){
+        let h2 = document.createElement(`h2`);
+        h2.classList.add(`text-shadow`, `title-case`);
+        h2.id = `${arrayOfObjects[i].name}-name`;
+        h2.textContent = arrayOfObjects[i].name;
+        let select = document.createElement(`select`);
+        select.name = `${arrayOfObjects[i].name}`;
+        select.id = `${arrayOfObjects[i].name}-dropdown`;
+        populateArr.push(h2);
+        for (let d of arrayOfObjects[i].drop){
+            let option = document.createElement(`option`);
+            option.classList.add(`title-case`);
+            option.value = d;
+            option.textContent = titleCase(d);
+            select.append(option);
+        }
+        parent = document.getElementById(`col${i + 1}`);
+        child = document.getElementById(`${arrayOfObjects[i].name}-image`);
+        populateArr.push(select);
+        parent.insertBefore(h2, child);
+        parent.insertBefore(select, child);
+        
+        imgArr[i] = document.getElementById(`${arrayOfObjects[i].name}-image`);
+        dropdown[i] = document.getElementById(`${arrayOfObjects[i].name}-dropdown`);
+
+        castleImage.innerHTML = `<img src="../assets/castle/${arrayOfObjects[0].drop[0]}.png" />`;
+        castleCount.textContent = castle;
+        downtownImage.innerHTML = `<img src="../assets/downtown/${arrayOfObjects[1].drop[0]}.png" />`;
+        downtownCount.textContent = downtown;
+        marsImage.innerHTML = `<img src="../assets/mars/${arrayOfObjects[2].drop[0]}.png" />`;
+        marsCount.textContent = mars;
+        
+        setTimeout(() => {
+            drop(i);
+        }, 300);
+        
     }
-    populateArr.push(select);
-    parent1.insertBefore(h2, child1);
-    parent1.insertBefore(select, child1);
 
-    castleDropdown = document.getElementById(`castle-dropdown`);
-    castleImage = document.getElementById(`castle-image`);
+    function drop(i) {
+        // let cas = dropdown[i];
+        dropdown[i].addEventListener('change', () => {
+            let state = arrayOfObjects[i].name;
+            setCount(state);
+            imgArr[i].innerHTML = `<img src="../assets/${arrayOfObjects[i].name}/${dropdown[i].value}.png" />`;
+            displayStats();
+        });
+    }
 
-    castleImage.innerHTML = `<img src="../assets/castle/${castleDropdown.value}.png" />`;
-    castleCount.textContent = castle;
-    downtownImage.innerHTML = `<img src="../assets/downtown/${downtownDropdown.value}.png" />`;
-    downtownCount.textContent = downtown;
-    marsImage.innerHTML = `<img src="../assets/mars/${marsDropdown.value}.png" />`;
-    marsCount.textContent = mars;
     let p = document.createElement(`p`);
     p.textContent = sloganArr;
     sloganShow.append(sloganArr);
+
 });
+
+function setCount(state){
+    switch (state) {
+        case `castle`:
+            castle++;
+            break;
+        case `downtown`:
+            downtown++;
+            break;
+        case `mars`:
+            mars++;
+            break;
+    }
+}
 
 //titlecase function stolen from google
 function titleCase(str) {
@@ -117,28 +146,6 @@ function titleCase(str) {
     }).join(' ');
 }
 
-//waits for page to load
-setTimeout(castleDrop, 200);
-function castleDrop() {
-    castleDropdown.addEventListener('change', () => {
-        castle++;
-        castleImage.innerHTML = `<img src="../assets/castle/${castleDropdown.value}.png" />`;
-        displayStats();
-    });
-}
-
-
-downtownDropdown.addEventListener('change', () => {
-    downtown++;
-    downtownImage.innerHTML = `<img src="../assets/downtown/${downtownDropdown.value}.png" />`;
-    displayStats();
-});
-  
-marsDropdown.addEventListener('change', () => {
-    mars++;
-    marsImage.innerHTML = `<img src="../assets/mars/${marsDropdown.value}.png" />`;
-    displayStats();
-});
 
 sloganButton.addEventListener(`click`, () => {
     let p = document.createElement(`p`);
